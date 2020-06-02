81efb69b359f5eec04f25df5e66b0e561c534142
Suranjan Moitra <suranjanbiswas@gmail.com>
2020-06-02 15:09:30 +0530

    Env Addded

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
 
   email: String,
   password: String
+  
 });
 
 userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
